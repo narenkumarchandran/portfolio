0418c04051bbe99f3d182e90d1ea9dc9faf72eb0
Narenkumar_C <152625215+narenkumarchandran@users.noreply.github.com>
2025-07-14 17:39:09 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/portfolio/', // 
+});
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
