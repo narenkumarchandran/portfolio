01b4f478544e3171c4b19c1432e8d572f99d83cc
Narenkumar_C <152625215+narenkumarchandran@users.noreply.github.com>
2025-07-14 18:40:40 +0530

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -84,7 +84,7 @@ const Hero = () => {
                 <div className="text-center">
                   <div className="absolute inset-4 rounded-full bg-white shadow-inner flex items-center justify-center overflow-hidden">
                     <img 
-                      src="/naren.jpeg" 
+                      src="./naren.jpeg" 
                       alt="Narenkumar" 
                        className="w-full h-full object-cover rounded-full"></img>
                   </div>
